d6089c95e2126a1e50c88ba1b4a90eed29ac4bbf
raikes <34956168+rex193@users.noreply.github.com>
2020-12-10 01:16:27 +0530

    completed  commit

---
+++ src/main/java/com/nirmal/demo/JpaWithJDBCApplication.java
@@ -0,0 +1,36 @@
+package com.nirmal.demo;
+
+
+import com.nirmal.demo.Entity.Person;
+import com.nirmal.demo.jdbc.PersonJdbcDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
+
+//@SpringBootApplication
+public class JpaWithJDBCApplication implements CommandLineRunner {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private PersonJdbcDao personJdbcDao;
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaWithJDBCApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("All user are -> {} ",personJdbcDao.findAll());
+        logger.info("User with id 10001 -> {} ",personJdbcDao.findById(10001));
+        logger.info("New User added with name -> {}",personJdbcDao.insert(
+                new Person(21,"Ritu","Cuttack",new Date())));
+        logger.info("User with id 10006 is deleted -> {}",personJdbcDao.deleteById(10006));
+
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,25 @@
+
+/*create table person
+(
+   id integer not null,
+   name varchar(255) not null,
+   location varchar(255),
+   birth_date timestamp,
+   primary key(id)
+);
+*/
+
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE )
+VALUES(10001,  'Ranga', 'Hyderabad',sysdate());
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE )
+VALUES(10002,  'James', 'New York',sysdate());
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE )
+VALUES(10003,  'Pieter', 'Amsterdam',sysdate());
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE )
+VALUES(10004,  'Nirmal', 'Bangalore',sysdate());
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE )
+VALUES(10005,  'Nihar', 'Bhubaneswar',sysdate());
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE )
+VALUES(10006,  'guru', 'Kolkata',sysdate());
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE )
+VALUES(10007,  'Niraj', 'bhopal',sysdate());
